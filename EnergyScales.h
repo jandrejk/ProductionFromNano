d871eb5cc4f57d42a2386b7e6aa4f69c1966dc0c
Janik Walter Andrejkovic <jandrejkovic@gmx.net>
2019-04-01 11:55:32 +0200

    adding 2018 TES

--- EnergyScales.h
+++ EnergyScales.h
@@ -1,13 +1,13 @@
 const struct ES{
 	struct Electron{
-		float oneProng0p0 = 0.003;
-		float oneProng1p0 = 0.036;
+		float oneProng0p0 = 0.0;
+		float oneProng1p0 = 0.0;
 		float threeProng0p0 = 0.0;
-		float uncertaintyShift1p   = 0.007;
-		float uncertaintyShift1p1p = 0.007;
-		float uncertaintyShift3p   = 0.03;
-		float uncertaintyBarrel    = 0.01;
-		float uncertaintyEndcap    = 0.025;
+		float uncertaintyShift1p   = 0.02;
+		float uncertaintyShift1p1p = 0.02;
+		float uncertaintyShift3p   = 0.02;
+		float uncertaintyBarrel    = 0.02;
+		float uncertaintyEndcap    = 0.02;
 	} Electron;
 
 	struct Muon{
@@ -18,13 +18,13 @@ const struct ES{
 		float uncertaintyShift1p1p = 0.02;
 		float uncertaintyShift3p   = 0.02;
 	} Muon;
-
+	// taken from here https://twiki.cern.ch/twiki/bin/viewauth/CMS/TauIDRecommendation13TeV
 	struct Tau{
-		float oneProng0p0 =   0.007;
-		float oneProng1p0 =  -0.002;
-		float threeProng0p0 = 0.001;
-		float uncertaintyShift1p   = 0.008;
-		float uncertaintyShift1p1p = 0.008;
-		float uncertaintyShift3p   = 0.009;
+		float oneProng0p0 =   -0.013;
+		float oneProng1p0 =   -0.005;
+		float threeProng0p0 = -0.012;
+		float uncertaintyShift1p   = 0.011;
+		float uncertaintyShift1p1p = 0.009;
+		float uncertaintyShift3p   = 0.008;
 	} Tau;
 } ES;
