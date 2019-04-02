560b80e4143ca50a36c80b3fa0456c4aef0755dc
MarkusSpanring <markusspanring@gmail.com>
2019-04-02 11:38:59 +0200

    change setup repo

--- submit_on_lxplus.sh
+++ submit_on_lxplus.sh
@@ -27,7 +27,7 @@ git clone https://github.com/cms-nanoAOD/nanoAOD-tools.git PhysicsTools/NanoAODT
 git clone https://github.com/svfit/ClassicSVfit.git TauAnalysis/ClassicSVfit -b fastMTT_21_06_2018
 git clone https://github.com/svfit/SVfitTF.git TauAnalysis/SVfitTF
 git clone https://github.com/CMS-HTT/RecoilCorrections.git  HTT-utilities/RecoilCorrections
-git clone https://github.com/jandrejk/ProductionFromNano.git WawTools/NanoAODTools -b SM2018
+git clone https://github.com/MarkusSpanring/ProductionFromNano.git WawTools/NanoAODTools -b SM2018
 cat FWCore/MessageLogger/interface/MessageDrop.h | sed s#CMS_THREAD_SAFE##g > FWCore/MessageLogger/interface/MessageDrop.h2
 mv FWCore/MessageLogger/interface/MessageDrop.h2 FWCore/MessageLogger/interface/MessageDrop.h 
 # compile
