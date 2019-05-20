40ef127e1d36f1977e6e9001aa54c7f7b1999f40
Janik Walter Andrejkovic <jandrejkovic@gmx.net>
2019-05-20 15:35:28 +0200

    TES adjusted

--- EnergyScales.h
+++ EnergyScales.h
@@ -20,11 +20,11 @@ const struct ES{
 	} Muon;
 	// taken from here https://twiki.cern.ch/twiki/bin/viewauth/CMS/TauIDRecommendation13TeV
 	struct Tau{
-		float oneProng0p0 =   -0.013;
+		float oneProng0p0 =   -0.006;
 		float oneProng1p0 =   -0.005;
-		float threeProng0p0 = -0.012;
-		float uncertaintyShift1p   = 0.011;
+		float threeProng0p0 =  0.000;
+		float uncertaintyShift1p   = 0.010;
 		float uncertaintyShift1p1p = 0.009;
-		float uncertaintyShift3p   = 0.008;
+		float uncertaintyShift3p   = 0.011;
 	} Tau;
 } ES;
